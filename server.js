41b53e98d69564ac7a6f21bfa142c7dfda349d4f
samuelbartels20 <bartelssamuel20@gmail.com>
2020-01-23 11:34:32 +0000

    adding ui

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const mail = require('./src/mail');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/poll-ui'));
+app.use(express.static(__dirname + '/dist/pollerrst'));
     
 
 app.use(bodyParser.json());
@@ -24,7 +24,7 @@ app.post('/sendmail', function(req,res) {
 });
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/poll-ui/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/pollerrst/index.html'));
     });
     
 
